d949c936e3a30b0b05384c67a09853d05db9c5bc
vitor Higor <carlosviniciussampaio@hotmail.com>
2024-02-13 19:43:55 -0300

    css finalizado

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
-
+  noticias = [
+   {
+    titulo: "Pandas em risco de extinção tomam o estado da China e promovem o Kung Fu gratuito nas fabricas de Iphone. Entenda!",
+    img: "https://dm0fehhuxv6f6.cloudfront.net/wp-content/uploads/2023/04/30035515/bing-panda-600x600.jpg",
+    autor: "Fernanda Lima"
+   },
+   {
+    titulo: "Pandas em risco de extinção tomam o estado da China e promovem o Kung Fu gratuito nas fabricas de Iphone. Entenda!",
+    img: "https://dm0fehhuxv6f6.cloudfront.net/wp-content/uploads/2023/04/30035515/bing-panda-600x600.jpg",
+    autor: "Fernanda Lima"
+   },
+   {
+    titulo: "Pandas em risco de extinção tomam o estado da China e promovem o Kung Fu gratuito nas fabricas de Iphone. Entenda!",
+    img: "https://dm0fehhuxv6f6.cloudfront.net/wp-content/uploads/2023/04/30035515/bing-panda-600x600.jpg",
+    autor: "Fernanda Lima"
+   },
+   {
+    titulo: "Pandas em risco de extinção tomam o estado da China e promovem o Kung Fu gratuito nas fabricas de Iphone. Entenda!",
+    img: "https://dm0fehhuxv6f6.cloudfront.net/wp-content/uploads/2023/04/30035515/bing-panda-600x600.jpg",
+    autor: "Fernanda Lima"
+   }
+  ]
 }
